50cd4d2521027506e4965108c637a799ffa40c98
DmitriyDodon2000 <dmitriydodonjob@gmail.com>
2020-09-06 16:07:42 +0300

    Create search.php

---
+++ project-template/cgi/search.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=9, initial-scale=1.0">
+	<title>Search</title>
+</head>
+<body>
+	<?php 
+		$filmName = $_REQUEST['filmSearch'];
+		echo "filmName";
+ 	 ?>
+</body>
+</html>
