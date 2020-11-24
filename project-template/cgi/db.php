9bd3abf370f1a55fdf395831735e514de81e64cf
DmitriyDodon <47043438+DmitriyDodon@users.noreply.github.com>
2020-11-24 16:19:19 +0200

    Update db.php

--- project-template/cgi/db.php
+++ project-template/cgi/db.php
@@ -1,6 +1,6 @@
 <?php 
 require 'libs/rb.php';
-R::setup( 'mysql:host=localhost;dbname=ICS-media','root', '' ); 
+R::setup( 'mysql:host=ec2-54-170-123-247.eu-west-1.compute.amazonaws.com:5432;dbname=dcp0isoi16tdma','frbgbjargntncg', 'f51cd8501cc1d3d8c410b56c0d4302427abb1f3c183f1d443220ac71fe83f6a4' ); 
 
 if ( !R::testconnection() )
 {
