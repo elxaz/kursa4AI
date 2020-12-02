d113b53b76d9749be27c9008d53a16739b19fc6e
DmitriyDodon <dumassick@gmail.com>
2020-12-02 23:35:06 +0200

    pay2

--- project-template/index.php
+++ project-template/index.php
@@ -1,3 +1,12 @@
-<script>
-	 location="cgi/show.php";
-</script>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="interkassa-verification" content="aa920cce22e1817b8276c5d623d176cc" />
+</head>
+<body>
+	<script>
+		 location="cgi/show.php";
+	</script>
+</body>
+</html>
+
